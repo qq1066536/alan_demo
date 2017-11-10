45c6e91629aa13c9b5caea67fc6ce8b856340a2d
alan <1066536184@qq.com>
2017-11-10 21:24:59 +0800

    update

--- func.js
+++ func.js
@@ -26,3 +26,43 @@ function getByClass(objParent,strClass){
 	}
 	return arrResult;
 }
+
+
+
+
+/**
+ * 不能实现暂停/继续效果
+ * @param {父元素} doms 
+ * @param {主体} body 
+ * @param {子元素} child 
+ * @param {移动速度} speed 
+ */
+var Marquee=function(doms,body,child,speed){
+	var mar_parent=doms;
+	var mar_body=mar_parent.getElementsByTagName(body)[0];
+	var mar_childs=mar_body.getElementsByTagName(child);
+	var timer=null;
+	this.speed=speed;
+	mar_body.innerHTML+=mar_body.innerHTML;
+	mar_body.style.width=mar_childs[0].offsetWidth*mar_childs.length+'px';
+
+	var move=function(){
+		if(mar_body.offsetLeft<-mar_body.offsetWidth/2){
+			mar_body.style.left='0';
+		}
+		if(mar_body.offsetLeft>0){
+			mar_body.style.left=-mar_body.offsetWidth/2+'px';
+		}
+		mar_body.style.left=mar_body.offsetLeft+speed+'px';
+	};
+	timer=setInterval(move,30);
+	var pauseMove=function(){
+		clearInterval(timer);
+	}
+	var cMove=function(){
+		timer=setInterval(move,30);
+	};
+	this.pauseMove=pauseMove;
+	this.cMove=cMove;
+
+};
--- 时钟效果/index.js
+++ 时钟效果/index.js
@@ -1,96 +1,98 @@
-
-window.onload=function(){
-    /**
+/**
      * 时钟主程序
      */
-Oclick=function(){
-    /**
-     * 
-     * @param {传入需要修改的DOM元素组} doms 
-     */
-    var init=function(doms){
-        (typeof doms.line_min=='undefined')?'':drawLines(doms.line_min,60,90);
-        (typeof doms.line_hour=='undefined')?'':drawLines(doms.line_hour,12,85);
-        (typeof doms.numbers=='undefined')?'':drawNumbers(doms.numbers,60,90);
-        move(doms);
-    };
-    /**
-     * 
-     * @param {dom元素} container 
-     * @param {绘制的总数} total 
-     * @param {X轴偏移量} translateX 
-     */
-    var drawLines=function(dom,total,translateX){
-        var gap=360/total;
-        var strHtml="";
-        for(var i =0 ; i<total;i++){
-            var newNode = document.createElement('li');
-            newNode.style.transform='rotate('+(i*gap)+'deg) translate('+translateX+'px,-50%)';
-            dom.appendChild(newNode);
-        }
-    };
-    /**
-     * 
-     * @param {dom元素} wrap 
-     */
-    var drawNumbers=function(dom){
-            var radius=window.getComputedStyle(dom).width.replace('px','')/2;
+    Oclick=function(){
+        /**
+         * 
+         * @param {传入需要修改的DOM元素组} doms 
+         */
+        var init=function(doms){
+            (typeof doms.line_min=='undefined')?'':drawLines(doms.line_min,60,90);
+            (typeof doms.line_hour=='undefined')?'':drawLines(doms.line_hour,12,85);
+            (typeof doms.numbers=='undefined')?'':drawNumbers(doms.numbers,60,90);
+            move(doms);
+        };
+        /**
+         * 
+         * @param {dom元素} container 
+         * @param {绘制的总数} total 
+         * @param {X轴偏移量} translateX 
+         */
+        var drawLines=function(dom,total,translateX){
+            var gap=360/total;
             var strHtml="";
-            for(var i = 1 ;i<=12 ;i++){
-                var myAngle=(i-3)/6*Math.PI;
-                var myX=radius+Math.cos(myAngle)*radius;
-                    myY=radius*(1+Math.sin(myAngle));
+            for(var i =0 ; i<total;i++){
                 var newNode = document.createElement('li');
-                newNode.style.left=myX+'px';
-                newNode.style.top=myY+'px';
-                newNode.innerHTML=i;       
+                newNode.style.transform='rotate('+(i*gap)+'deg) translate('+translateX+'px,-50%)';
                 dom.appendChild(newNode);
-
             }
-    };
-    /**
-     * 
-     * @param {指针的dom元素} doms 
-     */
-    var move=function(doms){
-        var domHour=doms.hour,
-            domMin=doms.minutes,
-            domSec=doms.secondes;
-        function showtime(){
-                var now=new Date(),
-                    hour=now.getHours(),
-                    min=now.getMinutes(),
-                    sec=now.getSeconds();
-                var secAngle=sec*6-90,
-                    minAngle=min*6+sec*0.1-90,
-                    hourAngle=hour*30+min*0.5-90;
-                rotate(domSec,secAngle);
-                rotate(domMin,minAngle);
-                rotate(domHour,hourAngle);
-                
+        };
+        /**
+         * 
+         * @param {dom元素} wrap 
+         */
+        var drawNumbers=function(dom){
+                var radius=window.getComputedStyle(dom).width.replace('px','')/2;
+                var strHtml="";
+                for(var i = 1 ;i<=12 ;i++){
+                    var myAngle=(i-3)/6*Math.PI;
+                    var myX=radius+Math.cos(myAngle)*radius;
+                        myY=radius*(1+Math.sin(myAngle));
+                    var newNode = document.createElement('li');
+                    newNode.style.left=myX+'px';
+                    newNode.style.top=myY+'px';
+                    newNode.innerHTML=i;       
+                    dom.appendChild(newNode);
+    
+                }
+        };
+        /**
+         * 
+         * @param {指针的dom元素} doms 
+         */
+        var move=function(doms){
+            var domHour=doms.hour,
+                domMin=doms.minutes,
+                domSec=doms.secondes;
+            function showtime(){
+                    var now=new Date(),
+                        hour=now.getHours(),
+                        min=now.getMinutes(),
+                        sec=now.getSeconds();
+                    var secAngle=sec*6-90,
+                        minAngle=min*6+sec*0.1-90,
+                        hourAngle=hour*30+min*0.5-90;
+                    rotate(domSec,secAngle);
+                    rotate(domMin,minAngle);
+                    rotate(domHour,hourAngle);
+                    
+                }
+            setInterval(showtime,1000);
+            showtime();
+        };
+        var rotate=function(dom, degrees) {
+            css(dom,{
+        '-webkit-transform' : 'rotate('+degrees+'deg)',
+            '-moz-transform' : 'rotate('+degrees+'deg)',  
+            '-ms-transform' : 'rotate('+degrees+'deg)',  
+            '-o-transform' : 'rotate('+degrees+'deg)',  
+                'transform' : 'rotate('+degrees+'deg)',  
+                    'zoom' : 1
+    
+            });
+        };
+        var css=function(dom,jsondata){
+            for(var i in jsondata){
+                dom.style[i]=jsondata[i];
             }
-        setInterval(showtime,1000);
-        showtime();
-    };
-    var rotate=function(dom, degrees) {
-        css(dom,{
-    '-webkit-transform' : 'rotate('+degrees+'deg)',
-        '-moz-transform' : 'rotate('+degrees+'deg)',  
-        '-ms-transform' : 'rotate('+degrees+'deg)',  
-        '-o-transform' : 'rotate('+degrees+'deg)',  
-            'transform' : 'rotate('+degrees+'deg)',  
-                'zoom' : 1
-
-        });
-    };
-    var css=function(dom,jsondata){
-        for(var i in jsondata){
-            dom.style[i]=jsondata[i];
-        }
+        };
+    
+        this.init=init;
     };
 
-    this.init=init;
-};
+    
+window.onload=function(){
+    
 /**
  * 获取dom元素
  */
